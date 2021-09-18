16ecba6045b86b6884819d871e2215666ec40fa0
Abu Saeid <abusaeid95@gmail.com>
2021-09-18 15:41:34 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+
+    </div>
+  );
+}
+
+function Person(){
+  return (
+    <div className="container">
+      <h1>Name: Shakib Al Hasan</h1>
+      <h4>Profession: Cricketer</h4>
     </div>
   );
 }
